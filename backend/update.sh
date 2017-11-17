f33698a352661d0fa173fc4ae70ca60747ea8e6e
Lauri Kangassalo <lauri.kangassalo@helsinki.fi>
2017-11-17 21:56:11 +0200

    Generate an archive of all the data files

--- backend/update.sh
+++ backend/update.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 mkdir -p data data-nasa
 mkdir -p public public-nasa
+OLD_STAT=$(stat -t data)
 python ftpfetch.py
-python parsehdf5.py
+NEW_STAT=$(stat -t data)
+if [[ $OLD_STAT != $NEW_STAT ]]; then
+    python parsehdf5.py
+    tar cjf public/latest.tar.bz2 public/*.json
+fi

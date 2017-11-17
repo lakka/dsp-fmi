a54edb08ff75d00d6c67a252b04d084b0bb722c0
Lauri Kangassalo <lauri.kangassalo@helsinki.fi>
2017-11-17 22:50:15 +0200

    Revert "Generate an archive of all the data files"
    This reverts commit f33698a352661d0fa173fc4ae70ca60747ea8e6e.

--- backend/update.sh
+++ backend/update.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 mkdir -p data data-nasa
 mkdir -p public public-nasa
-OLD_STAT=$(stat -t data)
 python ftpfetch.py
-NEW_STAT=$(stat -t data)
-if [[ $OLD_STAT != $NEW_STAT ]]; then
-    python parsehdf5.py
-    tar cjf public/latest.tar.bz2 public/*.json
-fi
+python parsehdf5.py

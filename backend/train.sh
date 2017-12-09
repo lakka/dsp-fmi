0435d00dc02f2aeb8c8c9ff59f900277d327cacc
Lauri Kangassalo <lauri.kangassalo@helsinki.fi>
2017-12-09 17:15:06 +0200

    Create symlinks for latest jsons

--- backend/train.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-nice -n 19 python traingaussian.py -p|ts '[%Y-%m-%d %H:%M:%S]' >> train.log
-rm .train.pid 2> /dev/null
--- backend/update.sh
+++ backend/update.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
+PIDFILE=".update.pid"
+if [[ -e $PIDFILE ]]; then
+    kill $(cat $PIDFILE)
+    rm $PIDFILE
+fi
+echo $BASHPID > $PIDFILE
+
+function symlinklatest {
+    ls -t $1/*.json|head -1|sed 's+.*/++'|xargs -I '{}' ln -fs '{}' $1/latest.json
+}
+
 mkdir -p data data-nasa
 mkdir -p public/predictions public-nasa
 STAT=$(stat -t data)
 python ftpfetch.py
 if [[ $STAT != $(stat -t data) ]]; then
     python parsehdf5.py
-    if [[ -e .train.pid ]]; then
-        kill $(cat .train.pid)
-        rm .train.pid
-    fi
-    nohup ./train.sh & > /dev/null
-    echo $! > .train.pid
+    symlinklatest "public"
+    nice -n 19 python traingaussian.py -p
+    symlinklatest "public/predictions"
 fi
+
+rm $PIDFILE

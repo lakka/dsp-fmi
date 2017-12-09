574954bff536dc4fa0e4cfbc1ebba2bf6ac28152
Lauri Kangassalo <lauri.kangassalo@helsinki.fi>
2017-12-09 12:49:17 +0200

    Run traingaussian on update

--- backend/update.sh
+++ backend/update.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 mkdir -p data data-nasa
-mkdir -p public public-nasa
+mkdir -p public/predictions public-nasa
+STAT=$(stat -t data)
 python ftpfetch.py
-python parsehdf5.py
+if [[ $STAT != $(stat -t data) ]]; then
+    python parsehdf5.py
+    nice -n 19 python traingaussian.py -p
+fi
